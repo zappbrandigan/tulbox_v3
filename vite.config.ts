e8798e7892d4420433f99796dff2e74f9acfb117
Brandon <brandlerson@gmail.com>
2025-06-26 08:14:23 -0500

    chore: update configs

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 });
