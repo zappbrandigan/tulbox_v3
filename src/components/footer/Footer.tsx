a00bdd69ba80ae50faf8dbcabfbd236f50ce5987
Brandon <brandlerson@gmail.com>
2025-07-03 12:44:24 -0500

    chore: update footer links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,20 +7,29 @@ export const Footer: React.FC = () => {
     day: 'numeric',
     year: 'numeric',
   });
-  const commit = __APP_COMMIT__;
+  const commit = __APP_COMMIT__.substring(0, 6);
 
   return (
     <footer className="w-full py-3 text-xs text-gray-500 flex flex-col items-center space-y-1">
       <div className="text-gray-400">
-        v{version} • {commit.substring(0, 6)}
+        v{version} • {commit}
       </div>
       <div className="text-gray-400">Last Updated {updated}</div>
-      <a
-        href="mailto:brandon@tulbox.app"
-        className="text-blue-500 hover:underline"
-      >
-        Send Feedback/Requests
-      </a>
+      <span>
+        <a
+          href={`mailto:brandon@tulbox.app?subject=Bug/Feedback:%20v${version}-${commit}`}
+          className="text-blue-500 hover:underline"
+        >
+          Feedback/Bug Report
+        </a>
+        {` • `}
+        <a
+          href={`mailto:brandon@tulbox.app?subject=Request:%20v${version}-${commit}`}
+          className="text-blue-500 hover:underline"
+        >
+          Requests
+        </a>
+      </span>
     </footer>
   );
 };
